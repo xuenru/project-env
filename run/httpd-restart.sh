2e49121a9eba38f0a4a6a190c7bad7ddf8babf37
XU Li <xuenru@gmail.com>
2017-12-01 22:57:37 +0100

    modify run bash on mac

--- run/httpd-restart.sh
+++ run/httpd-restart.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-. $PWD/httpd-stop.sh && . $PWD/httpd-start.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/httpd-stop.sh && . $DIR/httpd-start.sh
--- run/httpd-start.sh
+++ run/httpd-start.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run \
  --rm -d \
- -v $PWD/../htdocs/:/var/www/html/ \
- -v $PWD/../httpd/conf.d/:/etc/apache2/sites-enabled/ \
- -v $PWD/../httpd/logs/:/var/log/apache2/ \
+ -v $DIR/../htdocs/:/var/www/html/ \
+ -v $DIR/../httpd/conf.d/:/etc/apache2/sites-enabled/ \
+ -v $DIR/../httpd/logs/:/var/log/apache2/ \
  -p 80:80 --name xHttpd xer/php:7-apache
  
